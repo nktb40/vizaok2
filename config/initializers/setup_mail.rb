d4f5bf2d21f5e65040bce30770b7ff3f1368aba0
nktb40 <nktb40@gmail.com>
2014-01-29 02:08:22 +0400

    add images

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,9 +2,9 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 	  :address        => 'smtp.gmail.com',
 	  :port           => 587,
-	  :domain         => 'nktb40@gmail.com',
-	  :user_name      => 'nktb40',
-	  :password       => 'C72858nik472',
+	  :domain         => 'cardsharp70@gmail.com',
+	  :user_name      => 'cardsharp70',
+	  :password       => 'YTREWQ7ep',
 	  :authentication => 'plain',
      :enable_starttls_auto => true
 }
