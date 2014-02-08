315a9c29ee9f3fc1dd339d70836c8ae595987373
nktb40 <nktb40@gmail.com>
2014-02-08 18:55:25 +0400

    add yandex metrics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202123155) do
+ActiveRecord::Schema.define(version: 20140111193405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20140202123155) do
   create_table "questions", force: true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "phone"
     t.text     "question"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "phone"
   end
 
 end
