48aacdaddafbe12f4db5a157c86ef7cdc74d0d39
Nikita2 <nikita2@nikita-VirtualBox.(none)>
2014-03-22 19:00:14 -0400

    sideqik

--- app/Workers/question_mailer_worker.rb
+++ app/Workers/question_mailer_worker.rb
@@ -2,7 +2,7 @@ class QuestionMailerWorker
   include Sidekiq::Worker
   
   def perform(question_id)
-  	   @question = Question.find(order_id)
+  	   @question = Question.find(question_id)
   			QuestionMailer.send_question(@question, "help@vizaok.ru").deliver
 			QuestionMailer.send_question(@question, "nktb40@gmail.com").deliver
   end
