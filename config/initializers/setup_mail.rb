ee01c598cec67d2bb6a8675235885d1bad5ae90a
nktb40 <nktb40@gmail.com>
2014-04-02 23:53:47 +0400

    sss

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,11 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 	  :address        => 'smtp.yandex.ru',
-	  :port           => 25,
+	  :port           => 465,
 	  :domain         => 'help@vizaok.ru',
 	  :user_name      => 'help@vizaok.ru',
 	  :password       => '#YqMff95As',
-	  :authentication => 'plain',
-     :enable_starttls_auto => true
+	  :authentication => 'login',
+     :enable_starttls_auto => true,
+     :tls = > true
 }
